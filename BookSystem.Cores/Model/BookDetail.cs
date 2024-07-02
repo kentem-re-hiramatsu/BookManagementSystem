de27ff85b12ff263d9ea477507845ac0d8f9421f
re-hiramatsu <re-hiramatsu>
2024-07-02 12:00:21 +0900

    IsLendable 削除

--- BookSystem.Cores/Model/BookDetail.cs
+++ BookSystem.Cores/Model/BookDetail.cs
@@ -5,14 +5,12 @@
         public int TargetAge { get; set; }
         public string Author { get; set; }
         public string OverView { get; set; }
-        public bool IsLendable { get; set; }
 
         public BookDetail(int age, string author, string overView)
         {
             TargetAge = age;
             Author = author;
             OverView = overView;
-            IsLendable = true;
         }
     }
 }
