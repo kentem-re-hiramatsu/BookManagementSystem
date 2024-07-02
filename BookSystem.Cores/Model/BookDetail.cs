6deaa7500841ed6f5e48f968190f9f7614c7abf0
re-hiramatsu <re-hiramatsu>
2024-07-02 11:17:11 +0900

    本の詳細モデル実装

---
+++ BookSystem.Cores/Model/BookDetail.cs
@@ -0,0 +1,18 @@
+﻿namespace BookSystem.Cores.Model
+{
+    public class BookDetail
+    {
+        public int TargetAge { get; set; }
+        public string Author { get; set; }
+        public string OverView { get; set; }
+        public bool IsLendable { get; set; }
+
+        public BookDetail(int age, string author, string overView)
+        {
+            TargetAge = age;
+            Author = author;
+            OverView = overView;
+            IsLendable = true;
+        }
+    }
+}
