3977fa85e3111733d83e9c5cec1852d186107ec0
re-hiramatsu <re-hiramatsu>
2024-07-02 11:04:36 +0900

    絵本モデル実装

---
+++ BookSystem.Cores/Model/PictureBook.cs
@@ -0,0 +1,12 @@
+﻿namespace BookSystem.Cores.Model
+{
+    public class PictureBook : Book
+    {
+        public PictureBook(string title, BookDetail detail) : base(title, detail)
+        {
+            Type = "絵本";
+            Title = title;
+            Detail = detail;
+        }
+    }
+}
