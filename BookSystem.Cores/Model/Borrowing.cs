32e55d5f0ff715f5e5db918c9c9352436ce8ebb2
re-hiramatsu <re-hiramatsu>
2024-07-02 11:58:15 +0900

    貸出クラス実装

---
+++ BookSystem.Cores/Model/Borrowing.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BookSystem.Cores.Model
+{
+    public class Borrowing
+    {
+        public bool IsLendable { get; set; }
+        public DateTime? BorrowingTime { get; set; }
+        public DateTime? BorrowingPeriod { get; set; }
+
+        public Borrowing()
+        {
+            IsLendable = true;
+        }
+    }
+}
