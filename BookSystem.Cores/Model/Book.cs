8f887df1bdd058b60bbbf8df59ae06764ca72d9f
re-hiramatsu <re-hiramatsu>
2024-07-02 11:12:58 +0900

    book抽象クラス実装

---
+++ BookSystem.Cores/Model/Book.cs
@@ -0,0 +1,15 @@
+﻿namespace BookSystem.Cores.Model
+{
+    public abstract class Book
+    {
+        public string Title { get; set; }
+        public string Type { get; protected set; }
+        public BookDetail Detail { get; set; }
+
+        public Book(string title, BookDetail detail)
+        {
+            Title = title;
+            Detail = detail;
+        }
+    }
+}
