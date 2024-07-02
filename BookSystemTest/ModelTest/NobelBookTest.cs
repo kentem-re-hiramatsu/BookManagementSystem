f0d5dd24ceb013900681889469802e5e9d499747
re-hiramatsu <re-hiramatsu>
2024-07-02 11:11:48 +0900

    小説モデルのテスト作成

---
+++ BookSystemTest/ModelTest/NobelBookTest.cs
@@ -0,0 +1,21 @@
+﻿using BookSystem.Cores.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BookSystemTest.ModelTest
+{
+    [TestClass]
+    public class NobelBookTest
+    {
+        [TestMethod]
+        public void NobelTest()
+        {
+            var Nobel = new NovelBook("羅生門", new BookDetail(20, "芥川龍之介", "昔話"));
+
+            Assert.AreEqual("小説", Nobel.Type);
+            Assert.AreEqual("羅生門", Nobel.Title);
+            Assert.AreEqual(20, Nobel.Detail.TargetAge);
+            Assert.AreEqual("芥川龍之介", Nobel.Detail.Author);
+            Assert.AreEqual("昔話", Nobel.Detail.OverView);
+        }
+    }
+}
