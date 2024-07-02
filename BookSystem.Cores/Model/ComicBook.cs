6f46b250f6c47f74885077a7b2f7ac0fe54276fe
re-hiramatsu <re-hiramatsu>
2024-07-02 11:11:20 +0900

    漫画モデル実装

---
+++ BookSystem.Cores/Model/ComicBook.cs
@@ -0,0 +1,12 @@
+﻿namespace BookSystem.Cores.Model
+{
+    public class ComicBook : Book
+    {
+        public ComicBook(string title, BookDetail detail) : base(title, detail)
+        {
+            Type = "漫画";
+            Title = title;
+            Detail = detail;
+        }
+    }
+}
