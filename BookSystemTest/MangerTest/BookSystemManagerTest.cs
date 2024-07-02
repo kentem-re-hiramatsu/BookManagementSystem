96574b4eba5c7629122c542dbfed8409ad174c98
re-hiramatsu <re-hiramatsu>
2024-07-02 14:54:44 +0900

    BorrowingAndReturnProcess()削除

--- BookSystemTest/MangerTest/BookSystemManagerTest.cs
+++ BookSystemTest/MangerTest/BookSystemManagerTest.cs
@@ -1,7 +1,6 @@
 ﻿using BookSystem.Cores.Manager;
 using BookSystem.Cores.Model;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
 
 namespace BookSystemTest.MangerTest
 {
@@ -46,39 +45,6 @@ namespace BookSystemTest.MangerTest
 
             Assert.AreEqual("ドラゴンボール", bookSystemMana.Get(0).Title);
             Assert.AreEqual("鳥山明", bookSystemMana.Get(0).Detail.Author);
-
-        }
-
-        [TestMethod]
-        public void BorrowingAndReturnProcess()
-        {
-            var bookSystemMana = new BookSystemManager();
-
-            var onepice = new ComicBook("ワンピース", new BookDetail(10, "尾田栄一郎", ""), new Borrowing());
-
-            bookSystemMana.Add(onepice);
-
-            Assert.AreEqual(true, bookSystemMana.Get(0).Borrowing.IsLendable);
-
-            //貸出されていないためエラー
-            Assert.ThrowsException<Exception>(() => bookSystemMana.ReturnProcess(0));
-
-            bookSystemMana.BorrowingProcess(0);
-            var nowDateTime = DateTime.Now;
-            var fourteenDateTime = DateTime.Now.AddDays(14);
-
-            Assert.AreEqual(false, bookSystemMana.Get(0).Borrowing.IsLendable);
-            Assert.AreEqual(nowDateTime, bookSystemMana.Get(0).Borrowing.BorrowingTime);
-            Assert.AreEqual(fourteenDateTime, bookSystemMana.Get(0).Borrowing.BorrowingPeriod);
-
-            //返却されていないためエラー
-            Assert.ThrowsException<Exception>(() => bookSystemMana.BorrowingProcess(0));
-
-            bookSystemMana.ReturnProcess(0);
-
-            Assert.AreEqual(true, bookSystemMana.Get(0).Borrowing.IsLendable);
-            Assert.IsNull(bookSystemMana.Get(0).Borrowing.BorrowingTime);
-            Assert.IsNull(bookSystemMana.Get(0).Borrowing.BorrowingPeriod);
         }
     }
 }
