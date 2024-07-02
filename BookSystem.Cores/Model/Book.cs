846fe3dfe1ef426d7ab8bba8bfa61d5a2344bdb8
re-hiramatsu <re-hiramatsu>
2024-07-02 11:59:54 +0900

    Borrowing追加

--- BookSystem.Cores/Model/Book.cs
+++ BookSystem.Cores/Model/Book.cs
@@ -5,11 +5,13 @@
         public string Title { get; set; }
         public string Type { get; protected set; }
         public BookDetail Detail { get; set; }
+        public Borrowing Borrowing { get; set; }
 
-        public Book(string title, BookDetail detail)
+        public Book(string title, BookDetail detail, Borrowing borrowing)
         {
             Title = title;
             Detail = detail;
+            Borrowing = borrowing;
         }
     }
 }
