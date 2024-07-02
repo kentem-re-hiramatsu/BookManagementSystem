2e0a722c764d203e33553c971b8ee1a4f3c9b7e9
re-hiramatsu <re-hiramatsu>
2024-07-02 11:10:53 +0900

    漫画のテスト作成

---
+++ BookSystemTest/ModelTest/ComicBookTest.cs
@@ -0,0 +1,21 @@
+﻿using BookSystem.Cores.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BookSystemTest.ModelTest
+{
+    [TestClass]
+    public class ComicBookTest
+    {
+        [TestMethod]
+        public void ComicTest()
+        {
+            var Comic = new ComicBook("ワンピース", new BookDetail(12, "尾田栄一郎", "ルフィーを主人公とする物語。"));
+
+            Assert.AreEqual("漫画", Comic.Type);
+            Assert.AreEqual("ワンピース", Comic.Title);
+            Assert.AreEqual(12, Comic.Detail.TargetAge);
+            Assert.AreEqual("尾田栄一郎", Comic.Detail.Author);
+            Assert.AreEqual("ルフィーを主人公とする物語。", Comic.Detail.OverView);
+        }
+    }
+}
