eae4785caee3f0c7abf9e89144c72eabd8cbbdc1
re-hiramatsu <re-hiramatsu>
2024-07-02 11:12:03 +0900

    小説モデル実装

---
+++ BookSystem.Cores/Model/NovelBook.cs
@@ -0,0 +1,12 @@
+﻿namespace BookSystem.Cores.Model
+{
+    public class NovelBook : Book
+    {
+        public NovelBook(string title, BookDetail detail) : base(title, detail)
+        {
+            Type = "小説";
+            Title = title;
+            Detail = detail;
+        }
+    }
+}
