f8843779df79e06f4d51dcfc3f1648faee9a90a0
re-hiramatsu <re-hiramatsu>
2024-07-02 11:04:21 +0900

    絵本モデルテスト

---
+++ BookSystemTest/ModelTest/PictureBookTest.cs
@@ -0,0 +1,21 @@
+﻿using BookSystem.Cores.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BookSystemTest.ModelTest
+{
+    [TestClass]
+    public class PictureBookTest
+    {
+        [TestMethod]
+        public void PictureTest()
+        {
+            var picture = new PictureBook("ぐりとぐら", new BookDetail(0, "中川", "ぐりとぐらを主人公とする物語。"));
+
+            Assert.AreEqual("絵本", picture.Type);
+            Assert.AreEqual("ぐりとぐら", picture.Title);
+            Assert.AreEqual(0, picture.Detail.TargetAge);
+            Assert.AreEqual("中川", picture.Detail.Author);
+            Assert.AreEqual("ぐりとぐらを主人公とする物語。", picture.Detail.OverView);
+        }
+    }
+}
