01f1f585830d161c8cee2edb9af22f9eab4b42a3
re-hiramatsu <re-hiramatsu>
2024-07-03 16:09:38 +0900

    MainForm実装

--- BookManagementSystem/MainForm.cs
+++ BookManagementSystem/MainForm.cs
@@ -1,10 +1,55 @@
-﻿namespace BookManagementSystem
+﻿using BookSystem.Cores.Manager;
+using System.Windows.Forms;
+
+namespace BookManagementSystem
 {
-    public partial class EditForm : System.Windows.Forms.Form
+    public partial class MainForm : Form
     {
-        public EditForm()
+        private BookSystemManager _bookmana = new BookSystemManager();
+        public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void UpdateScreen()
+        {
+            BookListView.Items.Clear();
+            foreach (var book in _bookmana.BookOrderList)
+            {
+                BookListView.Items.Add(new ListViewItem(new string[] { book.Title, book.Type, book.Borrowing.GetLendingStatusByshape() }));
+            }
+        }
+
+        private void RegistrationButton_Click(object sender, System.EventArgs e)
+        {
+            if (new RegistrationForm(_bookmana).ShowDialog() == DialogResult.OK)
+            {
+                UpdateScreen();
+            }
+        }
+
+        private void EditButton_Click(object sender, System.EventArgs e)
+        {
+            if (new EditForm(_bookmana).ShowDialog() == DialogResult.OK)
+            {
+                UpdateScreen();
+            }
+        }
+
+        private void DetailButton_Click(object sender, System.EventArgs e)
+        {
+            if (new DetailForm(_bookmana).ShowDialog() == DialogResult.OK)
+            {
+                UpdateScreen();
+            }
+        }
+
+        private void BorrowingButton_Click(object sender, System.EventArgs e)
+        {
+            if (new BorrowingForm(_bookmana).ShowDialog() == DialogResult.OK)
+            {
+                UpdateScreen();
+            }
         }
     }
 }
