1c79248bd2f802e773ccb98858f5a6a6817426e7
re-hiramatsu <re-hiramatsu>
2024-07-03 14:58:48 +0900

    GetBookInstanceTestの実装

--- BookSystemTest/MangerTest/BookSystemManagerTest.cs
+++ BookSystemTest/MangerTest/BookSystemManagerTest.cs
@@ -1,4 +1,5 @@
-﻿using BookSystem.Cores.Manager;
+﻿using BookSystem.Cores.Enum;
+using BookSystem.Cores.Manager;
 using BookSystem.Cores.Model;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -14,8 +15,8 @@ namespace BookSystemTest.MangerTest
 
             Assert.AreEqual(0, bookSystemMana.BookOrderList.Count);
 
-            var onepice = new ComicBook("ワンピース", new BookDetail(10, "尾田栄一郎", ""), new Borrowing());
-            var guriToGura = new PictureBook("ぐりとぐら", new BookDetail(0, "ぐり", ""), new Borrowing());
+            var onepice = new ComicBook("ワンピース", new BookDetail(10, "尾田栄一郎", ""));
+            var guriToGura = new PictureBook("ぐりとぐら", new BookDetail(0, "ぐり", ""));
 
             bookSystemMana.Add(onepice);
             bookSystemMana.Add(guriToGura);
@@ -35,7 +36,7 @@ namespace BookSystemTest.MangerTest
         {
             var bookSystemMana = new BookSystemManager();
 
-            var onepice = new ComicBook("ワンピース", new BookDetail(10, "尾田栄一郎", ""), new Borrowing());
+            var onepice = new ComicBook("ワンピース", new BookDetail(10, "尾田栄一郎", ""));
 
             bookSystemMana.Add(onepice);
 
@@ -45,6 +46,36 @@ namespace BookSystemTest.MangerTest
 
             Assert.AreEqual("ドラゴンボール", bookSystemMana.Get(0).Title);
             Assert.AreEqual("鳥山明", bookSystemMana.Get(0).Detail.Author);
+        }
+
+        [TestMethod]
+        public void GetBookInstanceTest()
+        {
+            var bookSystemMana = new BookSystemManager();
+
+            var picture = bookSystemMana.GetBookInstance(BookType.絵本, "ぐりとぐら", new BookDetail(10, "ぐり", ""));
+            var comic = bookSystemMana.GetBookInstance(BookType.漫画, "ワンピース", new BookDetail(15, "尾田栄一郎", ""));
+            var nobel = bookSystemMana.GetBookInstance(BookType.小説, "ぐり", new BookDetail(0, "ぐら", ""));
+
+            Assert.AreEqual(BookType.絵本, picture.Type);
+            Assert.AreEqual(BookType.漫画, comic.Type);
+            Assert.AreEqual(BookType.小説, nobel.Type);
+
+            Assert.AreEqual("ぐりとぐら", picture.Title);
+            Assert.AreEqual("ワンピース", comic.Title);
+            Assert.AreEqual("ぐり", nobel.Title);
+
+            Assert.AreEqual(10, picture.Detail.TargetAge);
+            Assert.AreEqual(15, comic.Detail.TargetAge);
+            Assert.AreEqual(0, nobel.Detail.TargetAge);
+
+            Assert.AreEqual("ぐり", picture.Detail.Author);
+            Assert.AreEqual("尾田栄一郎", comic.Detail.Author);
+            Assert.AreEqual("ぐら", nobel.Detail.Author);
+
+            Assert.AreEqual("", picture.Detail.OverView);
+            Assert.AreEqual("", comic.Detail.OverView);
+            Assert.AreEqual("", nobel.Detail.OverView);
         }
     }
 }
