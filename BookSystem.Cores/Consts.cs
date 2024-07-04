0ae3b9310d00e4ca312cf26f4dd316dfcafdbf48
re-hiramatsu <re-hiramatsu>
2024-07-04 15:09:47 +0900

    本の対象年齢以下だった場合のエラーメッセージを定数に追加

--- BookSystem.Cores/Consts.cs
+++ BookSystem.Cores/Consts.cs
@@ -7,6 +7,7 @@
         public const string OVER_LOAN_PEROD_ERROR_MESSAGE = "貸出期限は２週間以内です";
         public const string ON_LOAN_ERROR_MESSAGE = "貸出されているため貸し出すことができません";
         public const string NOT_LOAN_ERROR_MESSAGE = "貸出がされていません";
+        public const string AGE_ERROR_MESSAGE = "本の対象年齢以下のため貸出ができません";
 
         public const string LOANABLE_MESSAGE = "貸出可";
         public const string ON_LOAN_MESSAGE = "貸出中";
