a62c7cc7b98eab7d6bb5ad08f87c33fda24017e3
re-hiramatsu <re-hiramatsu>
2024-07-04 13:42:32 +0900

    Userテスト実装

---
+++ BookSystemTest/ModelTest/UserInfoTest.cs
@@ -0,0 +1,19 @@
+﻿using BookSystem.Cores.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BookSystemTest.ModelTest
+{
+    [TestClass]
+    public class UserInfoTest
+    {
+        [TestMethod]
+        public void GetUserInfoTest()
+        {
+            var user = new User("けん", 20, true);
+
+            Assert.AreEqual("けん", user.Name);
+            Assert.AreEqual(20, user.Age);
+            Assert.IsTrue(user.IsAdmin);
+        }
+    }
+}
