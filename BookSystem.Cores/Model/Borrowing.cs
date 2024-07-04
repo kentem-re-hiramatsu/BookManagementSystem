b63505ffab35dd8513e14113b398abfd99d2b59e
re-hiramatsu <re-hiramatsu>
2024-07-04 15:27:26 +0900

    Borrowing.csのメンバ変数にUserを追加

--- BookSystem.Cores/Model/Borrowing.cs
+++ BookSystem.Cores/Model/Borrowing.cs
@@ -4,6 +4,7 @@ namespace BookSystem.Cores.Model
 {
     public class Borrowing
     {
+        public User User { get; set; }
         public bool IsLendable { get; set; }
         public DateTime? BorrowingTime { get; set; }
         public DateTime? DeadlineDateTime { get; set; }
