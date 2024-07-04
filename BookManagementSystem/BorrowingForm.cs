2c65d9e1b3e8aed6da6a9eadcd063bb01b096b0b
re-hiramatsu <re-hiramatsu>
2024-07-04 10:11:39 +0900

    BorrowingForm実装

--- BookManagementSystem/BorrowingForm.cs
+++ BookManagementSystem/BorrowingForm.cs
@@ -1,12 +1,90 @@
-﻿using System.Windows.Forms;
+﻿using BookSystem.Cores.Manager;
+using System;
+using System.Windows.Forms;
 
 namespace BookManagementSystem
 {
     public partial class BorrowingForm : Form
     {
-        public BorrowingForm()
+        private BookSystemManager _bookmana;
+
+        public BorrowingForm(BookSystemManager bookMana)
         {
             InitializeComponent();
+            _bookmana = bookMana;
+        }
+
+        private void BorrowingForm_Load(object sender, EventArgs e)
+        {
+            ReturnDateTimePicker.Value = DateTime.Now;
+            ReturnDateTimePicker.MinDate = DateTime.Now;
+            UpdateScreen();
+        }
+
+        private void UpdateScreen()
+        {
+            BookListView.Items.Clear();
+            foreach (var book in _bookmana.BookOrderList)
+            {
+                BookListView.Items.Add(new ListViewItem(new string[] { book.Title, book.Type, book.Borrowing.GetLendingStatusByshape() }));
+            }
+        }
+
+        private void CloseButton_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.OK;
+        }
+
+        public int GetSelectedIndex()
+        {
+            if (BookListView.SelectedItems.Count > 0)
+                return BookListView.SelectedItems[0].Index;
+            return -1;
+        }
+
+        private void LendButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (BookListView.SelectedItems.Count > 0)
+                {
+                    _bookmana.Get(GetSelectedIndex()).Borrowing.SetBorrowing(ReturnDateTimePicker.Value);
+                    UpdateScreen();
+                }
+                ButtonEnableChenged();
+            }
+            catch (Exception m)
+            {
+                MessageBox.Show(m.Message, "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void ReturnButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (BookListView.SelectedItems.Count > 0)
+                {
+                    _bookmana.Get(GetSelectedIndex()).Borrowing.SetReturn();
+                    UpdateScreen();
+                }
+                ButtonEnableChenged();
+            }
+            catch (Exception m)
+            {
+                MessageBox.Show(m.Message, "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void ButtonEnableChenged()
+        {
+            LendButton.Enabled = BookListView.SelectedItems.Count > 0;
+            ReturnButton.Enabled = BookListView.SelectedItems.Count > 0;
+        }
+
+        private void BookListView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ButtonEnableChenged();
         }
     }
 }
