8cd3eec17937c614a4b08b1a6cf6a33918b70a5e
re-hiramatsu <re-hiramatsu>
2024-07-04 15:59:04 +0900

    貸出先、年齢、権限を表示機能追加

--- BookManagementSystem/DetailForm.cs
+++ BookManagementSystem/DetailForm.cs
@@ -1,4 +1,6 @@
-﻿using BookSystem.Cores.Manager;
+﻿using BookSystem.Cores.Enum;
+using BookSystem.Cores.Manager;
+using BookSystem.Cores.Model;
 using System.Windows.Forms;
 
 namespace BookManagementSystem
@@ -7,17 +9,20 @@ namespace BookManagementSystem
     {
         private BookSystemManager _bookmana;
         private int _selectedIndex;
+        private User _user;
 
-        public DetailForm(BookSystemManager bookMana, int selectedIndex)
+        public DetailForm(BookSystemManager bookMana, int selectedIndex, User user)
         {
             InitializeComponent();
             _bookmana = bookMana;
             _selectedIndex = selectedIndex;
+            _user = user;
         }
 
         private void DetailForm_Load(object sender, System.EventArgs e)
         {
             var book = _bookmana.Get(_selectedIndex);
+
             BookListView.Items.Add(new ListViewItem(new string[] {
                 book.Type,
                 book.Title,
@@ -26,6 +31,23 @@ namespace BookManagementSystem
                 book.Detail.OverView,
                 book.Borrowing.GetLendingStatus()
             }));
+
+            var user = book.Borrowing.User;
+
+            if (book.Borrowing.User.IsAdmin)
+            {
+                UserType admin;
+                if (user.IsAdmin)
+                    admin = UserType.管理者;
+                else
+                    admin = UserType.一般;
+
+                UserInfoListView.Items.Add(new ListViewItem(new string[] { user.Name, user.Age.ToString(), admin.ToString() }));
+            }
+            else
+            {
+                UserInfoListView.Items.Add(new ListViewItem(new string[] { user.Name, user.Age.ToString() }));
+            }
         }
 
         private void OkButton_Click(object sender, System.EventArgs e)
