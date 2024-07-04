69ef5fc83cde59ada875d4e0fe2bd4c2733699b4
re-hiramatsu <re-hiramatsu>
2024-07-04 13:42:57 +0900

    Userモデル実装

---
+++ BookSystem.Cores/Model/User.cs
@@ -0,0 +1,16 @@
+﻿namespace BookSystem.Cores.Model
+{
+    public class User
+    {
+        public string Name { get; }
+        public int Age { get; }
+        public bool IsAdmin { get; }
+
+        public User(string name, int age, bool isAdmin)
+        {
+            Name = name;
+            Age = age;
+            IsAdmin = isAdmin;
+        }
+    }
+}
