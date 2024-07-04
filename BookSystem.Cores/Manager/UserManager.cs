fa0d13c796cbe0a23ec5fbff62fc4ae6204e7b61
re-hiramatsu <re-hiramatsu>
2024-07-04 14:01:09 +0900

    UserManager実装

---
+++ BookSystem.Cores/Manager/UserManager.cs
@@ -0,0 +1,22 @@
+﻿using BookSystem.Cores.Model;
+using System.Collections.Generic;
+
+namespace BookSystem.Cores.Manager
+{
+    public class UserManager
+    {
+        private List<User> _userDatas = new List<User>();
+
+        public IReadOnlyCollection<User> UserDatas { get { return _userDatas; } }
+
+        public void Add(User user)
+        {
+            _userDatas.Add(user);
+        }
+
+        public User Get(int index)
+        {
+            return _userDatas[index];
+        }
+    }
+}
