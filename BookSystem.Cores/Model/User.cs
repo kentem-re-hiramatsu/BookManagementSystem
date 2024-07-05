cf1f106d666465fe1058f9c964df13f1fe7fad0a
re-hiramatsu <re-hiramatsu>
2024-07-05 14:17:00 +0900

    User.csにIsRestrictionプロパティー追加

--- BookSystem.Cores/Model/User.cs
+++ BookSystem.Cores/Model/User.cs
@@ -5,12 +5,14 @@
         public string Name { get; }
         public int Age { get; }
         public bool IsAdmin { get; }
+        public bool IsRestriction { get; set; }
 
-        public User(string name, int age, bool isAdmin)
+        public User(string name, int age, bool isAdmin, bool isRestriction)
         {
             Name = name;
             Age = age;
             IsAdmin = isAdmin;
+            IsRestriction = isRestriction;
         }
     }
 }
