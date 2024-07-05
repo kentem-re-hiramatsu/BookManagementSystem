da9b5ed4f8162fc04a928700759f4ea9022cba7d
re-hiramatsu <re-hiramatsu>
2024-07-05 16:59:59 +0900

    ユーザー管理画面で制限解除機能実装

--- BookManagementSystem/UserInfoForm.cs
+++ BookManagementSystem/UserInfoForm.cs
@@ -19,17 +19,30 @@ namespace BookManagementSystem
             foreach (var user in _userMana.UserDatas)
             {
                 var admin = UserType.一般;
+                var restriction = UserType.一般ユーザー;
 
                 if (user.IsAdmin)
                     admin = UserType.管理者;
+                if (user.IsRestriction)
+                    restriction = UserType.制限ユーザー;
 
-                UserInfoListView.Items.Add(new ListViewItem(new string[] { user.Name, user.Age.ToString(), admin.ToString() }));
+                UserInfoListView.Items.Add(new ListViewItem(new string[] { user.Name, user.Age.ToString(), admin.ToString(), restriction.ToString() }));
             }
         }
 
         private void CloseButton_Click(object sender, System.EventArgs e)
         {
             Close();
+        }
+
+        private void RestrictionButton_Click(object sender, System.EventArgs e)
+        {
+            _userMana.Get(UserInfoListView.SelectedItems[0].Index).IsRestriction = false;
+        }
+
+        private void UserInfoListView_SelectedIndexChanged(object sender, System.EventArgs e)
+        {
+            RestrictionButton.Enabled = UserInfoListView.SelectedItems.Count > 0;
         }
     }
 }
